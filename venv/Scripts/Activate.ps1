f01187d0492fbb263633c7775ee43ada0b96f2f1
LukasJerabek <55019231+LukasJerabek@users.noreply.github.com>
2019-10-17 23:54:57 +0200

    changing directory

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="D:\Documents\Dropbox\PycharmProjects\flaskblogmy\venv"
+$env:VIRTUAL_ENV="D:\Documents\Disk Google\PycharmProjects\flaskblogmy\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
--- venv/Scripts/pip-script.py
+++ venv/Scripts/pip-script.py
@@ -1,4 +1,4 @@
-#!D:\Documents\Dropbox\PycharmProjects\flaskblogmy\venv\Scripts\python.exe
+#!"D:\Documents\Disk Google\PycharmProjects\flaskblogmy\venv\Scripts\python.exe"
 # EASY-INSTALL-ENTRY-SCRIPT: 'pip==19.0.3','console_scripts','pip'
 __requires__ = 'pip==19.0.3'
 import re
